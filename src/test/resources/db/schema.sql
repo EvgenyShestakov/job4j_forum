10ed48a2e9e6fe1e9247ba58937846e35d381336
ultar <lester_g@inbox.ru>
2022-03-09 13:02:56 +0300

    Spring Liquibase.

--- src/test/resources/db/schema.sql
+++ src/test/resources/db/schema.sql
@@ -9,3 +9,6 @@ id serial PRIMARY KEY,
 message TEXT,
 posts_id int NOT NULL REFERENCES posts(id)
 );
+
+insert into posts (name) values ('О чем этот форум?');
+insert into posts (name) values ('Правила форума.');
